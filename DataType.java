1edc1df781883322f4664a7c7c5dc5bbb8cfe209
Tojo-Tom <tomtojo2003@gmail.com>
2026-06-28 20:31:54 +0530

    Lab cycle Task - 4

---
+++ DataType.java
@@ -0,0 +1,22 @@
+    public class DataType {
+        public static void main(String[] args){
+            int a = 100;
+            float f = 25.5f;
+            double d = 123.456;
+            char  c = 'A';
+            long l = 1000000000L;
+            boolean b = true;
+            byte bb = 127;
+            short  s = 32000;
+
+            System.out.println("Integer :" +a);
+            System.out.println("Long : "+l);
+            System.out.println("float : "+f);
+            System.out.println("Double : "+ d);
+            System.out.println("Charecter : "+c);
+            System.out.println("Boolean : "+b);
+            System.out.println("Byte : "+bb);
+            System.out.println("Short : "+s);
+
+        }
+    }
