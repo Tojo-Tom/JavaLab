9946d611d9ee6252d7fa21f350149cc0782cdc26
Tojo-Tom <tomtojo2003@gmail.com>
2026-06-30 09:42:32 +0530

    Lab cycle Task - 18

---
+++ Pattern.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Pattern {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.print("Rows = ");
+        int rows = scanner.nextInt();
+
+        System.out.println("\nOutput");
+
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+    }
+}
